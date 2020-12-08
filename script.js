2f38304ee92635ba601bd8933a25a88402a6313c
AndreyBarreto <andreyvbarreto@gmail.com>
2020-12-07 21:05:27 -0300

    Javascript

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
     console.log(data)
     const generateHTML = data.products.map((api)=>{
     
+
+
+        
+  
+    
     
            
         
         return `
             <div class="js">
-            <a><img class="image" src=${api.image}></a>
-            <p class="productname" >${api.name}</p>
-            <p class="description" >${api.description}</p>
-            <p class="oldprice" >${api.oldPrice}</p>
-            <p class="price" >${api.price}</p>
-            <p class="installments" >${api.installments}</p>
-            <button class="compra"></button>
+                <a><img class="image" src=${api.image}></a>
+                <p class="productname" >${api.name}</p>
+                <p class="description" >${api.description}</p>
+                <p class="oldprice" >De: R$${api.oldPrice}</p>
+                <p class="price" >Por: R$${api.price}</p>
+                <p class="installments" >ou ${api.installments.count}x de R$${api.installments.value}</p>
+                <button class="compra">Comprar</button>
             </div>
                 
            
@@ -28,3 +33,36 @@ fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
         
         
 });
+
+function myFunction(){
+
+    fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=2")
+.then(function(response) {
+    return response.json();
+  })
+.then(function(data) {
+    console.log(data)
+    const generateHTML = data.products.map((api)=>{ 
+    
+        return `
+            <div class="js">
+                <a><img class="image" src=${api.image}></a>
+                <p class="productname" >${api.name}</p>
+                <p class="description" >${api.description}</p>
+                <p class="oldprice" >De: R$${api.oldPrice}</p>
+                <p class="price" >Por: R$${api.price}</p>
+                <p class="installments" >ou ${api.installments.count}x de R$${api.installments.value}</p>
+                <button class="compra">Comprar</button>
+            </div>
+                
+           
+        `
+    })
+        .join("")
+        document.getElementById("cards2").innerHTML=generateHTML
+ 
+        
+        
+});
+
+}
